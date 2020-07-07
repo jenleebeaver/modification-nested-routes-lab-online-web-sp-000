df85f82b8015e32acbfd62d1a4a9e1f973f5d3de
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-07 17:26:08 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,9 @@
 
 module SongsHelper
 
-  def display_artist(song)
-   song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
-  end
+  # def display_artist(song)
+  #  song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+  # end
 
   # Display dropdown of artists, with name if editing through nested
   def artist_select(song)
